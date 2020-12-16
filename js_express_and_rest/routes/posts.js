4bddf4540cfe4e5fec1869b6df0fe49bdaa18e53
Brandon Y Lam <me@brandonlam.ca>
2020-12-16 10:47:51 -0800

    add post show page

--- js_express_and_rest/routes/posts.js
+++ js_express_and_rest/routes/posts.js
@@ -17,6 +17,22 @@ router.get('/new', (req, res) => {
   res.render('posts/new');
 });
 
+// In this url, all the words prefixed with : are called URL params.
+// You can have any value as the url params and it can be accessed via the name provided
+router.get('/:id', (req, res) => {
+// we can grab the id using req.params.id
+// Example:
+// If we have a url like '/:id/:blue/:red and someone visits this url http://localhost:3000/posts/hello/20/5
+// our req.params will look like { id: 'hello', blue: 20, red: 5}
+  knex('posts')
+    .where('id', req.params.id)
+    .first()
+    // .first() is a method built into knex it will return only the first record of the array of records from a query
+    .then(post => {
+      res.render('posts/show', { post:post });
+    });
+});
+
 // Create
 router.post('/', (req, res) => {
   knex('posts')
