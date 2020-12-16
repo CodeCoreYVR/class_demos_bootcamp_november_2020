45ad08ddb5cf0a1bc19dec9c1ac8225653d18f3d
Brandon Y Lam <me@brandonlam.ca>
2020-12-16 10:25:41 -0800

    add posts index

--- js_express_and_rest/routes/posts.js
+++ js_express_and_rest/routes/posts.js
@@ -3,10 +3,21 @@ const knex = require('../db/client');
 
 const router = express.Router();
 
+// Index
+router.get('/', (req, res) => {
+  knex('posts')
+    .orderBy('created_at', 'desc')
+    .then(posts => {
+      res.render('posts/index', { posts: posts });
+    });
+});
+
+// New
 router.get('/new', (req, res) => {
   res.render('posts/new');
 });
 
+// Create
 router.post('/', (req, res) => {
   knex('posts')
     .insert({
