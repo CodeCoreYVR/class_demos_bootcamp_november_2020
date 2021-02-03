b9a072e7baebbbff9534af44f8004b6623a19ed2
Naveed Raza <naveedraza@gmail.com>
2021-02-02 16:09:11 -0800

    lab 4

--- labs/js-event-propagation-and-jQuery/js/js/main.js
+++ labs/js-event-propagation-and-jQuery/js/js/main.js
@@ -81,5 +81,29 @@ $('#form-1 > input[type=text]').on('input', function(event) {
 // actually add the form validation, if you wish
 
 $('#form-1 > input[type=text]').attr('maxlength', 14);
+// Lab 4
+$('#form-1').submit(function(e) {
+	const color = $('#form-1 > input[type=text]').val();
+
+	// If they enter an invalid color show them an alert message telling them,
+	// "Entered color is not a valid option!"
+	const validColors = ['red', 'blue', 'black', 'grey'];
+
+	if (!validColors.includes(color)) {
+		alert(`Entered colour, "${color}", is not a valid option!`);
+	} else {
+		// All shapes matching the given color should be removed.
+		$(`.shape.${color}`).remove();
+	}
+
+	// Clear the input field.
+	this.reset(); /* 
+		This really clears the entire form, but since the form only has one input,
+		it is equivalent.
+	*/
+	// or
+	// $('#form-1 > input[type=text]').val('');
+});
+
 
 });// Bump
