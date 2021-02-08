b55a45d9103d0e2705d6bb7c88a405d88f67f174
Brandon Y Lam <me@brandonlam.ca>
2021-02-08 12:16:33 -0800

    add show page

--- js_spa/scripts/main.js
+++ js_spa/scripts/main.js
@@ -34,6 +34,10 @@ const Questions = {
       },
       body: JSON.stringify(params)
     }).then(res => res.json())
+  },
+  show(id) {
+    return fetch(`${BASE_URL}/questions/${id}`)
+      .then(res => res.json())
   }
 }
 
@@ -47,13 +51,24 @@ function renderQuestions(questions) {
   questionsContainer.innerHTML = questions.map(q => {
     return `
       <li>
-        <a class="question-link" href="#">
+        <a class="question-link" href="#" data-id="${q.id}">
           <span>${q.id} - </span>
           ${q.title}
         </a>
       </li>
     `
   }).join('');
+}
+
+function renderQustionShow(q) {
+  const showPage = document.querySelector('#question-show');
+  showPage.innerHTML = `
+    <h1>${q.title}</h1>
+    <p>${q.body}</p>
+    <small>${q.author ? q.author.first_name : ''}</small>
+    <small>${q.like_count}</small>
+  `
+  navigateTo('question-show')
 }
 
 function navigateTo(id) {
@@ -110,4 +125,31 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log(page);
     navigateTo(page)
   })
+
+  // Show page logic
+  const questionsContainer = document.querySelector('ul.question-list');
+  questionsContainer.addEventListener('click', (event) => {
+    event.preventDefault();
+    const target = event.target.closest('a');
+    if (target) {
+      const id = target.dataset.id;
+      console.log(id);
+      Questions.show(id)
+        .then(q => {
+          renderQustionShow(q);
+        })
+    }
+  })
 })
+
+// {id: 29, title: "We need to parse the wireless thx array!", body: "Chuck Norris doesn't believe in floating point num…cause they can't be typed on his binary keyboard.", created_at: "2019-05-06T00:00:00.000Z", updated_at: "2019-05-06T00:00:00.000Z", …}
+// answers: (4) [{…}, {…}, {…}, {…}]
+// author: {id: 9, first_name: "Chi", last_name: "Jacobi", full_name: "Chi Jacobi"}
+// body: "Chuck Norris doesn't believe in floating point numbers because they can't be typed on his binary keyboard."
+// created_at: "2019-05-06T00:00:00.000Z"
+// id: 29
+// like_count: 5
+// randomstuff: "You can add anything to your json response, This is an example"
+// title: "We need to parse the wireless thx array!"
+// updated_at: "2019-05-06T00:00:00.000Z"
+// view_count: 22652
